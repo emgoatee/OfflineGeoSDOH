7236d99720d5bc2e7e6a3b2736abbef3d890e3cc
emgoatee <ecemtombas@gmail.com>
2026-01-26 14:13:45 -0500

    Fix build script echo

--- build_installer.sh
+++ build_installer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-echo "Building Offline GEO-SDOH v1.1.1 Installer"
+echo "Building Offline GEO-SDOH v$VERSION Installer"
 echo "==========================================="
 echo ""
 
